c3006e36f9c7b114dd24b4c22e3dc164b0661a26
Mane-ops <122042767@upq.edu.mx>
2024-05-27 20:13:52 -0600

    Rutas con Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 
 app= Flask(__name__)
+
+# Paso 1: Crear una Ruta Simple
+@app.route('/')
+def principal():
+    return 'Hola Mundo Flask!'
+
+# Paso 2: Crear una Ruta Doble
+@app.route('/usuario')
+@app.route('/saludar')
+def saludos():
+    return 'Hola Mane Rosellón'
+
+# Paso 3: Crear Rutas con Parámetros
+@app.route('/hi/<nombre>')
+def hi(nombre):
+    return 'Hola '+ nombre + '!!!'
+
+# Paso 4: Definición de métodos de trabajo
+@app.route('/formulario',methods=['GET','POST'] )
+def formulario():
+    if request.method == 'GET':
+        return 'No es seguro enviar passwords por GET'
+    elif request.method == 'POST':
+        return 'POST si es seguro para passwords' 
+    
+# Paso 5: Manejo de excepciones para rutas
+@app.errorhandler(404)
+def paginano(e):
+    return 'Revisa tu sintaxis: No encontré nada'
 
 if __name__ == '__main__':
     app.run(port=3000, debug= True)
