59241a285417638fa76be1825135a7fed89ad009
Mane-ops <122042767@upq.edu.mx>
2024-05-31 20:22:08 -0600

    Practica 5 Avance

---
+++ CRUD_Albums/app.py
@@ -0,0 +1,32 @@
+from flask import Flask,request,render_template
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'bdflask'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/guardarAlbum',methods=['POST'])
+def guardarAlbum():
+    if request.method == 'POST':
+        titulo = request.form['txtTitulo']
+        artista = request.form['txtArtista']
+        anio = request.form['txtAnio']
+        print(titulo,artista,anio)
+        return 'Datos recibidos en el Server'
+
+# Manejo de excepciones para rutas
+@app.errorhandler(404)
+def paginano(e):
+    return 'Revisa tu sintaxis: No encontré nada'
+
+if __name__ == '__main__':
+    app.run(port=3000,debug=True)
